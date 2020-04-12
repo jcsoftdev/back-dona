ea037095747cac583d8101c9e340d0d9b61bb30f
jcsoftia <ajcarlos032@gmail.com>
2020-04-12 17:41:52 -0500

    config

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ process.env.PORT = process.env.PORT || 5000
 
 //URI MONGO
 
-const URI_MONGO = 'mongodb://PeruSoftware:y5MsaFdkeOWpcF7K@perusoftware-shard-00-00-c73aw.gcp.mongodb.net:27017,perusoftware-shard-00-01-c73aw.gcp.mongodb.net:27017,perusoftware-shard-00-02-c73aw.gcp.mongodb.net:27017/test?ssl=true&replicaSet=PeruSoftware-shard-0&authSource=admin&retryWrites=true&w=majority'
+const URI_MONGO = 'mongodb://PeruSoftware:y5MsaFdkeOWpcF7K@perusoftware-shard-00-00-c73aw.gcp.mongodb.net:27017,perusoftware-shard-00-01-c73aw.gcp.mongodb.net:27017,perusoftware-shard-00-02-c73aw.gcp.mongodb.net:27017/donaPeru?ssl=true&replicaSet=PeruSoftware-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 process.env.URI_MONGO = URI_MONGO
 
